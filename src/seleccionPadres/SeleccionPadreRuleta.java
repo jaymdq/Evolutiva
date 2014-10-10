e890ea2f882a6a98ad198e24dd1969218f1f844c
Sebastián Vallejos <sebaviru@gmail.com>
2014-10-10 17:57:52 -0300

    Selecion de padres por ruleta

--- src/seleccionPadres/SeleccionPadreRuleta.java
+++ src/seleccionPadres/SeleccionPadreRuleta.java
@@ -10,8 +10,23 @@ public class SeleccionPadreRuleta implements SeleccionPadres {
 	
 	@Override
 	public Vector<Vector<Cromosoma>> seleccionar(Vector<Cromosoma> conjunto) {
-		// TODO Auto-generated method stub
-		return null;
+		Double total = 0.0;
+		for (Cromosoma c: conjunto)
+			total += c.getFitness();
+		
+		Vector<Vector<Cromosoma>> salida = new Vector<Vector<Cromosoma>>();
+		
+		while (salida.size() != conjunto.size()/2) {
+			Vector<Cromosoma> pareja = new Vector<Cromosoma>();
+			Double rand = Math.random() * total;
+			int pos = 0;
+			while (conjunto.get(pos).getFitness() < rand) {
+				rand -= conjunto.get(pos).getFitness();
+				pos++;
+			}
+			salida.add(pareja);
+		}
+		return salida;
 	}
 
 	@SuppressWarnings("static-access")
